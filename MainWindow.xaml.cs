4612ca1de80b69942639350aa171ead439dd3ac1
Andrey Martin <jeffersonandreyma12@gmail.com>
2022-04-26 12:22:10 -0500

    Added button functions(SOME)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,194 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        string number1 = "";
+        string number2 = "";
+        string operation = "";
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btn_0_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 += "" + 0;
+                txt_ToShow.Text = number1;
+            }
+            else 
+            {
+                number2 += "" + 0;
+                txt_ToShow.Text = number2;
+            }
+        }
+
+        private void btn_1_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 += "" + 1;
+                txt_ToShow.Text = number1;
+            }
+            else
+            {
+                number2 += "" + 1;
+                txt_ToShow.Text = number2;
+            }
+        }
+
+        private void btn_2_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 += "" + 2;
+                txt_ToShow.Text = number1;
+            }
+            else
+            {
+                number2 += "" + 2;
+                txt_ToShow.Text = number2;
+            }
+        }
+
+        private void btn_3_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 += "" + 3;
+                txt_ToShow.Text = number1;
+            }
+            else
+            {
+                number2 += "" + 3;
+                txt_ToShow.Text = number2;
+            }
+        }
+
+        private void btn_4_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 += "" + 4;
+                txt_ToShow.Text = number1;
+            }
+            else
+            {
+                number2 += "" + 4;
+                txt_ToShow.Text = number2;
+            }
+        }
+
+        private void btn_5_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 += "" + 5;
+                txt_ToShow.Text = number1;
+            }
+            else
+            {
+                number2 += "" + 5;
+                txt_ToShow.Text = number2;
+            }
+        }
+
+        private void btn_6_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 += "" + 6;
+                txt_ToShow.Text = number1;
+            }
+            else
+            {
+                number2 += "" + 6;
+                txt_ToShow.Text = number2;
+            }
+        }
+
+        private void btn_7_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 += "" + 7;
+                txt_ToShow.Text = number1;
+            }
+            else
+            {
+                number2 += "" + 7;
+                txt_ToShow.Text = number2;
+            }
+        }
+
+        private void btn_8_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 += "" + 8;
+                txt_ToShow.Text = number1;
+            }
+            else
+            {
+                number2 += "" + 8;
+                txt_ToShow.Text = number2;
+            }
+        }
+
+        private void btn_9_Click(object sender, RoutedEventArgs e)
+        {
+            if (operation == "")
+            {
+                number1 += "" + 9;
+                txt_ToShow.Text = number1;
+            }
+            else
+            {
+                number2 += "" + 9;
+                txt_ToShow.Text = number2;
+            }
+        }
+
+        private void btn_Sum_Click(object sender, RoutedEventArgs e)
+        {
+            operation = "+";
+        }
+
+        private void ntn_Subtraction_Click(object sender, RoutedEventArgs e)
+        {
+            operation = "-";
+        }
+
+        private void btn_Multiplication_Click(object sender, RoutedEventArgs e)
+        {
+            operation = "*";
+        }
+
+        private void btn_Division_Click(object sender, RoutedEventArgs e)
+        {
+            operation = "/";
+        }
+
+        private void btn_Result_Click(object sender, RoutedEventArgs e)
+        {
+            switch (operation)
+            {
+                case "+":
+                    txt_ToShow.Text = (Convert.ToInt32(number1)+ Convert.ToInt32(number2)).ToString();
+                    break;
+                case "-":
+
+                    break;
+
+                case "*":
+
+                    break;
+
+                case "/":
+
+                    break;
+            }
         }
     }
 }
